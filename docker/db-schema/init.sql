2ac1461032c6fad537cb4b1bea26171bea7519b9
bfs <barteksemeniuk57@gmail.com>
2022-01-23 16:43:37 +0100

    migration

--- docker/db-schema/init.sql
+++ docker/db-schema/init.sql
@@ -1,12 +1,1 @@
 create schema test_db;
-
-create table test_db.person (
-    id int not null,
-    name varchar(100) not null,
-    constraint pk_person primary key (id)
-);
-
-insert into test_db.person(id, name) VALUES
-(
- 1, 'test name'
-)
