91796843990a47c9cde84fdcffddd4ee3d595d49
Bartlomiej Mazurski <s20479@pjwstk.edu.pl>
2022-03-10 14:35:10 +0100

    Renamed person to user

--- src/main/java/com/example/io_backend/controller/PersonController.java
+++ src/main/java/com/example/io_backend/controller/PersonController.java
@@ -1,7 +1,7 @@
 package com.example.io_backend.controller;
 
 import com.example.io_backend.exception.NotFoundException;
-import com.example.io_backend.model.Person;
+import com.example.io_backend.model.User;
 import com.example.io_backend.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -9,35 +9,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController()
+@RequestMapping(path = "/person")
 @RequiredArgsConstructor
 public class PersonController {
     private final PersonRepository personRepository;
 
-    @GetMapping("/person")
-    public List<Person> getAll() {
+    @GetMapping("")
+    public List<User> getAll() {
         return personRepository.findAll();
     }
 
-    @GetMapping("/person/{id}")
-    public Person getById(@PathVariable Integer id) {
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Integer id) {
         return personRepository.findById(id).orElseThrow(() -> new NotFoundException("No record with that id"));
     }
 
-    @PostMapping("/person")
-    public Person add(@RequestBody Person person) {
-        return personRepository.save(person);
+    @PostMapping("")
+    public User add(@RequestBody User user) {
+        return personRepository.save(user);
     }
 
-    @PutMapping("/person/{id}")
-    public void update(@RequestBody Person person, @PathVariable Integer id) {
+    @PutMapping("/{id}")
+    public void update(@RequestBody User user, @PathVariable Integer id) {
         var p = personRepository.findById(id).orElseThrow(() -> new NotFoundException("Person not found"));
-        p.setId(person.getId());
-        p.setName(person.getName());
+        p.setId(user.getId());
+        p.setName(user.getName());
 
         personRepository.save(p);
     }
 
-    @DeleteMapping("/person/{id}")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable Integer id) {
         var p = personRepository.findById(id).orElseThrow(() -> new NotFoundException("Person not found"));
         personRepository.delete(p);
