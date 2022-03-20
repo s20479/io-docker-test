f4399564f730a0107d2b0e645d28356339729a67
Bartosz Semeniuk <s20462@pjwstk.edu.pl>
2022-03-20 21:40:37 +0100

    cleanup

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build -f ./docker/Dockerfile -t pjatk/io_backend:latest .
-docker-compose -f ./docker/docker-compose.yml down && docker-compose -f ./docker/docker-compose.yml up
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-while ! nc -z database 3306
-do
-  sleep 2
-done
-
-java -jar app.jar
