590f7f6740b1288611fe76cd88b57591058d5e90
Bartosz Semeniuk <s20462@pjwstk.edu.pl>
2022-06-23 18:44:26 +0200

    Blood type update (#46)
    * quick fix
    
    * updated blood types enum

--- src/main/java/com/example/io_backend/model/enums/BloodType.java
+++ src/main/java/com/example/io_backend/model/enums/BloodType.java
@@ -1,7 +1,9 @@
 package com.example.io_backend.model.enums;
 
 public enum BloodType {
-    A_PLUS, AB_PLUS, O_PLUS,
-    A_MINUS, AB_MINUS, O_MINUS,
+    A_PLUS,A_MINUS,
+    O_PLUS, O_MINUS,
+    B_PLUS, B_MINUS,
+    AB_PLUS, AB_MINUS,
     UNKNOWN
 }
